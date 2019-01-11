88c3fd3211916e04fab2840b7bf0cc0ee3fe3c5b
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-01-11 23:44:12 +0530

    Added class Paddle
    paddle class takes height, width, bottom, left and
    have functions moveLeft, moveRight

--- src/game.js
+++ src/game.js
@@ -3,3 +3,19 @@ const sum = function (a, b) {
 };
 
 
+class Paddle {
+  constructor(height, width, bottom, left){
+    this.height = height;
+    this.width = width;
+    this.bottom = bottom;
+    this.left = left;
+  }
+
+  moveLeft(){
+    this.left -= 10;
+  }
+
+  moveRight() {
+    this.left += 10;
+  }
+}
