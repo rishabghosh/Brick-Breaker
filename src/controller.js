9dd63481783f947a0f334daa78cbe2fa91e70208
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-01-12 09:27:25 +0530

    Implimentd drawPaddle
    drawPaddle is called inside initialize.
    drawPaddle changes spec(height, width, bottom, left) of
    id paddle_1 div by takeing same specs of paddle object
    which is a instance of Paddle class

--- src/controller.js
+++ src/controller.js
@@ -5,11 +5,23 @@ const createPaddleDiv = function(){
   return div;
 };
 
+const applyPixel = (count) => count + "px";
+
+const drawPaddle = function(paddle){
+  const paddleDiv = document.getElementById("paddle_1");
+  paddleDiv.style.height = applyPixel(paddle.height);
+  paddleDiv.style.width = applyPixel(paddle.width);
+  paddleDiv.style.left = applyPixel(paddle.left);
+  paddleDiv.style.bottom = applyPixel(paddle.bottom);
+};
 
 const initialize = function () {
   const playground = document.getElementById("playground");
   const paddleDiv = createPaddleDiv();
   playground.appendChild(paddleDiv);
+
+  const paddle = new Paddle(20,100,5,430);
+  drawPaddle(paddle);
   
   console.log(paddleDiv);
   console.log("window loaded");
